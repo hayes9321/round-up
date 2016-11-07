79e10bc0c73b1363617498fe6b60efd32aec5a38
alfordsiler <alfordsiler@gmail.com>
2016-11-07 15:09:32 -0800

    added models for candidate, interview, position, question

---
+++ models/interview.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+
+var interviewSchema = new mongoose.Schema({
+  candidate: { type: mongoose.Schema.Types.ObjectId, ref: 'Candidate'},
+  question: { type: mongoose.Schema.Types.ObjectId, ref: 'Question'},
+  position: { type: mongoose.Schema.Types.ObjectId, ref: 'Position'},
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+  score: Number,
+  notes: String
+});
+
+var Interview = mongoose.model('Interview', interviewSchema);
+
+module.exports = Interview;
