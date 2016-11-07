10716eb9596ce099efd682b1b9d6acd6422576bb
britbroet <brittanybroetje@gmail.com>
2016-11-07 12:48:28 -0800

    more framework and auth

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,24 @@
-angular.module('Roundup', []);
+angular.module('Roundup', ['ui.router']);
+.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider){
+	$urlRouterProvider.otherwise('/'); // usually goes to a 404 route
+
+	$stateProvider
+	.state('home', {
+		url: '/',
+		templateUrl: 'app/views/home.html',
+		controller: 'HomeCtrl'
+	})
+	// .state('post', {
+	// 	url: '/post/:id',
+	// 	templateUrl: 'app/views/post.html',
+	// 	controller: 'PostCtrl'
+	// })
+	// .state('edit-post', {
+	// 	url: '/post/:id/edit',
+	// 	templateUrl: 'app/views/post_edit.html',
+	// 	controller: 'PostCtrl'
+	// });
+
+	$locationProvider.html5Mode(true);
+
+}]); 
