b3a0911d87531975bd57e30331355912b99f6d6c
hayes9321 <t.hayes8321@gmail.com>
2016-11-08 12:15:10 -0800

    saving info for merge

--- public/js/app/services.js
+++ public/js/app/services.js
@@ -25,7 +25,7 @@ angular.module('Roundup')
   this.getAllCandidates = function(callback) {
     $http({
       url: '/api/candidates',
-
+  }
 .service('QuestionService', ['$http', function($http) {
   this.getAllQuestions = function(callback) {
     $http({
@@ -55,70 +55,70 @@ angular.module('Roundup')
 //   return $resource('/api/users/:id');
 // }])
 
-.factory('Auth', ['$window', function($window) {
-  return {
-    saveToken: function(token) {
-      $window.localStorage['secretrecipes-token'] = token;
-    },
-    getToken: function() {
-      return $window.localStorage['secretrecipes-token'];
-    },
-    removeToken: function() {
-      $window.localStorage.removeItem('secretrecipes-token');
-    },
-    isLoggedIn: function() {
-      var token = this.getToken();
-      return token ? true : false;
-    },
-    currentUser: function() {
-      if (this.isLoggedIn()) {
-        var token = this.getToken();
-        try {
-          var payload = JSON.parse($window.atob(token.split('.')[1]));
-          return payload;
-        } catch(err) {
-          return false;
-        }
-      }
-    }
-  }
-}])
+// .factory('Auth', ['$window', function($window) {
+//   return {
+//     saveToken: function(token) {
+//       $window.localStorage['secretrecipes-token'] = token;
+//     },
+//     getToken: function() {
+//       return $window.localStorage['secretrecipes-token'];
+//     },
+//     removeToken: function() {
+//       $window.localStorage.removeItem('secretrecipes-token');
+//     },
+//     isLoggedIn: function() {
+//       var token = this.getToken();
+//       return token ? true : false;
+//     },
+//     currentUser: function() {
+//       if (this.isLoggedIn()) {
+//         var token = this.getToken();
+//         try {
+//           var payload = JSON.parse($window.atob(token.split('.')[1]));
+//           return payload;
+//         } catch(err) {
+//           return false;
+//         }
+//       }
+//     }
+//   }
+// }])
 
-.factory('Alerts', [function() {
-  var alerts = [];
+// .factory('Alerts', [function() {
+//   var alerts = [];
 
-  return {
-    clear: function() {
-      alerts = [];
-    },
-    add: function(type, msg) {
-      alerts.push({type: type, msg: msg});
-    },
-    get: function() {
-      return alerts;
-    },
-    remove: function(idx) {
-      alerts.splice(idx, 1);
-    }
-  }
-}])
+//   return {
+//     clear: function() {
+//       alerts = [];
+//     },
+//     add: function(type, msg) {
+//       alerts.push({type: type, msg: msg});
+//     },
+//     get: function() {
+//       return alerts;
+//     },
+//     remove: function(idx) {
+//       alerts.splice(idx, 1);
+//     }
+//   }
+// }])
 
-.factory('AuthInterceptor', ['Auth', function(Auth) {
-  // if querying other APIs, add URLs to this array
-  var excludedEndpoints = [
-    'https://swapi.co/api/films'
-  ];
+// .factory('AuthInterceptor', ['Auth', function(Auth) {
+//   // if querying other APIs, add URLs to this array
+//   var excludedEndpoints = [
+//     'https://swapi.co/api/films'
+//   ];
 
-  return {
-    request: function(config) {
-      var token = Auth.getToken();
-      var excludedEndpoint = excludedEndpoints.indexOf(config.url) > -1;
-      if (token && !excludedEndpoint) {
-        config.headers = config.headers || {};
-        config.headers.Authorization = 'Bearer ' + token;
-      }
-      return config;
-    }
-  }
-}]);
+//   return {
+//     request: function(config) {
+//       var token = Auth.getToken();
+//       var excludedEndpoint = excludedEndpoints.indexOf(config.url) > -1;
+//       if (token && !excludedEndpoint) {
+//         config.headers = config.headers || {};
+//         config.headers.Authorization = 'Bearer ' + token;
+//       }
+//       return config;
+//     }
+//   }
+// }]);
 
