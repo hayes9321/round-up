e5bd7103aac23f18b42540026db3abbb67151660
alfordsiler <alfordsiler@gmail.com>
2016-11-08 09:24:21 -0800

    added navbar

---
+++ public/js/app/components/navBar/navbar.js
@@ -0,0 +1,13 @@
+(function() {
+  angular.module('Roundup')
+  .component('navBar', {
+    templateUrl: 'js/app/components/navbar/navbar.html',
+    controller: Navbar,
+    controllerAs: 'navbar'
+  });
+
+  function Navbar() {
+    console.log("navbar")
+  }
+
+})()
