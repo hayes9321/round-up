60c5e0110c9c8c49408c6740c6d1b48af2cfbff6
hayes9321 <t.hayes8321@gmail.com>
2016-11-08 16:40:40 -0800

    still working on auth

--- public/js/app/components/navBar/navbar.js
+++ public/js/app/components/navBar/navbar.js
@@ -2,12 +2,28 @@
   angular.module('Roundup')
   .component('navBar', {
     templateUrl: 'js/app/components/navbar/navbar.html',
-    controller: Navbar,
-    controllerAs: 'navbar'
+    controller: NavBar,
+    controllerAs: 'navBar'
   });
 
-  function Navbar() {
-    console.log("navbar")
+  function NavBar($http, $state, $location, Auth, Alerts) {
+    var navbar = this;
+    navbar.Auth = Auth;
+    console.log("this is Auth",  Auth)
+
+    navbar.logout = function() {
+    Auth.removeToken();
+    Alerts.add('success', 'Logged out!');
+    $state.reload();
+  	}
   }
 
+  NavBar.$inject = ['$http','$state', '$location', 'Auth','Alerts'];
 })()
+
+  // $scope.Auth = Auth;
+  // $scope.logout = function() {
+  //   Auth.removeToken();
+  //   Alerts.add('success', 'Logged out!');
+  //   $state.reload();
+  // }
