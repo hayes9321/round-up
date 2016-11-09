4d90a6aec8f6e385e68553661c1742127413527b
alfordsiler <alfordsiler@gmail.com>
2016-11-09 12:58:06 -0800

    add candidates almost complete

--- controllers/candidates.js
+++ controllers/candidates.js
@@ -5,9 +5,17 @@ var router = express.Router();
 //Find a candidate
 router.get('/', function(req, res) {
   Candidate.find(function(err, candidates) {
-    console.log("trying to find all candidates")
     if (err) return res.send({message: 'An error occurred when finding candidate'});
     res.send(candidates);
+  });
+});
+
+router.post('/', function(req, res) {
+  var candidate = new Candidate(req.body);
+  candidate.save(function(err) {
+    if (err) return res.send({message: 'An error occurred when creating a candidate'});
+    res.send(candidate);
+    console.log(candidate);
   });
 });
 
--- public/js/app/components/newCandidateForm/newCandidateForm.js
+++ public/js/app/components/newCandidateForm/newCandidateForm.js
@@ -2,12 +2,28 @@
   angular.module('Roundup')
   .component('newCandidateForm', {
     templateUrl: 'js/app/components/newCandidateForm/newCandidateForm.html',
-    controller: newCandidateForm,
+    controller: NewCandidateForm,
     controllerAs: 'newCandidateForm'
   });
 
-  function newCandidateForm() {
+  function NewCandidateForm($state, CandidateService) {
     var newCandidateForm = this;
-    console.log('newCandidateForm firing');
+    newCandidateForm.candidates = [];
+    newCandidateForm.newCandidate = {
+      firstName: '',
+      lastName: ''
+    };
+
+  newCandidateForm.submitCandidate = function() {
+    CandidateService.addCandidate(newCandidateForm.newCandidate, function(data) {
+      CandidateService.getAllCandidates(function(data) {
+        newCandidateForm.candidates = data.data;
+        console.log("new candidate data: ", data.data);
+      });
+    });
   }
-})();
+}
+
+NewCandidateForm.$inject = ['$state', 'CandidateService'];
+
+})()
