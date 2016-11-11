8ff1c89f34578b1e78c59b4cb8448f8fea2fa87c
britbroet <brittanybroetje@gmail.com>
2016-11-11 09:23:29 -0800

    cleanup and automatically adding questions based on position

--- public/js/app/components/newRoundForm/newRoundForm.js
+++ public/js/app/components/newRoundForm/newRoundForm.js
@@ -21,7 +21,8 @@
       position: {
         title: '',
         description: ''
-      }
+      },
+      questions: []
     };
 
     CandidateService.getAllCandidates(function(data) {
@@ -43,18 +44,33 @@
 
     newRoundForm.addPosition = function() {
       PositionService.getPosition(newRoundForm.selectedPosition, function(res) {
+        console.log('position questions?', res.data.questions);
+
+        var existingQuestions = res.data.questions;
+        var compileQuestions = [];
+        console.log('questions: ', compileQuestions);
+
+        existingQuestions.forEach(function(question){
+          compileQuestions.push({question});
+          console.log('new round with new questions: ', newRoundForm.newRound);
+          console.log('compile questions: ', compileQuestions);
+          newRoundForm.newRound.questions = compileQuestions;
+        });
+
         newRoundForm.position = res.data;
         newRoundForm.newRound.position.title = newRoundForm.position.jobTitle;
         newRoundForm.newRound.position.description = newRoundForm.position.description;
+        newRoundForm.newRound.position.questions = compileQuestions;
         console.log('position data added: ', newRoundForm.newRound);
       });
     }
 
     newRoundForm.createRound = function() {
       RoundService.addRound(newRoundForm.newRound, function(data) {
+        console.log('newRoundForm.newRound: ', newRoundForm.newRound);
         var newRoundId = data.data._id;
         window.location.href = '/round/' + newRoundId;
-        console.log('new round added: ', data.data);
+        //console.log('new round added: ', data.data);
       }); 
     }
 
